0d4853511ebe31d298a477e5b45b9d7eb267565f
Dominic Walsh <djcwalsh94@gmail.com>
2022-10-27 15:33:05 +0200

    good momentum

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,16 @@
+class BookmarksController < ApplicationController
+
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+    @movies = Movie.all
+  end
+
+  def create
+    
+  end
+
+
+  def destroy
+  end
+end
