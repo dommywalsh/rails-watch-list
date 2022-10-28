7aad7a17a84ae8abe5ccdeae2c1f9df206d00065
Dominic Walsh <djcwalsh94@gmail.com>
2022-10-28 17:03:39 +0200

    aaaaaaaaa

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,39 +1,23 @@
-puts "Cleaning database..."
-Movie.destroy_all
-List.destroy_all
+require "json"
+require "open-uri"
+
+puts 'Clearing DB'
 Bookmark.destroy_all
+Movie.destroy_all
 
-Movie.create(title: "Wonder Woman 1984",
-  overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s",
-  poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg",
-  rating: 6.9
-)
+url = "https://tmdb.lewagon.com/movie/top_rated"
+data_serialized = URI.open(url).read
+data = JSON.parse(data_serialized)
 
-Movie.create(title: "The Shawshank Redemption",
-   overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison",
-    poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg",
-     rating: 8.7
-    )
+relevant_infos = data["results"]
 
-Movie.create(title: "Titanic",
-   overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.",
-    poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg",
-    rating: 7.9
-    )
+relevant_infos.each do |info|
+  poster_url = "https://image.tmdb.org/t/p/original/#{info["poster_path"]}"
+  file = URI.open(poster_url)
+  movie = Movie.new(title: info["original_title"], overview: info["overview"], poster_url: poster_url, rating: info["popularity"])
+  movie.photo.attach(io: file, filename: "#{info["original_title"].gsub(" ", "-")}.png", content_type: "image/png")
+  movie.save
+end
 
-Movie.create(title: "Ocean's Eight",
-   overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.",
-    poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg",
-    rating: 7.0
-  )
-
-List.create(name: "horror")
-List.create(name: "drama")
-List.create(name: "comedy")
-
-40.times do
-  Bookmark.create(
-    comment: 'Want to watch these movies',
-    movie: Movie.all.sample,
-    list: List.all.sample)
-  end
+puts 'Finished!'
+puts "Made #{Movie.count} movies"
