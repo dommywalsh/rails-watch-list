f1a18207a0376111a58c3c608f932baa6cc2c9af
Dominic Walsh <djcwalsh94@gmail.com>
2022-10-28 11:44:25 +0200

    new

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -7,10 +7,27 @@ class BookmarksController < ApplicationController
   end
 
   def create
-    
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
 
   def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list)
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :lists, only: [:index, :show, :create, :new] do
-    resources :bookmarks, only: [:create, :new, :destroy]
+    resources :bookmarks, only: [:create, :new]
   end
+  resources :bookmarks, only: [:destroy]
 end
