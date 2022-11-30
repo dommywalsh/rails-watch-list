b99d8b8ab0f1392e5216a00bf4068ebac092ef1f
Dominic Walsh <djcwalsh94@gmail.com>
2022-11-30 16:24:48 +0200

    fixing index and show pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root to: "lists#index"
+  resources :movie, only: [:show]
   resources :lists, only: [:index, :show, :create, :new] do
     resources :bookmarks, only: [:create, :new]
+    resources :reviews, only: :create
   end
   resources :bookmarks, only: [:destroy]
+  resources :reviews, only: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,7 @@ require "open-uri"
 puts 'Clearing DB'
 Bookmark.destroy_all
 Movie.destroy_all
+List.destroy_all
 
 url = "https://tmdb.lewagon.com/movie/top_rated"
 data_serialized = URI.open(url).read
@@ -14,7 +15,11 @@ relevant_infos = data["results"]
 relevant_infos.each do |info|
   poster_url = "https://image.tmdb.org/t/p/original/#{info["poster_path"]}"
   file = URI.open(poster_url)
-  movie = Movie.new(title: info["original_title"], overview: info["overview"], poster_url: poster_url, rating: info["popularity"])
+  movie = Movie.new(
+    title: info["original_title"],
+    overview: info["overview"],
+    poster_url: poster_url,
+    rating: info["popularity"])
   movie.photo.attach(io: file, filename: "#{info["original_title"].gsub(" ", "-")}.png", content_type: "image/png")
   movie.save
 end
